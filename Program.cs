5fa5c0e2f6c7a008106aea301bfae24617e29c91
felipe <165731375+felipelopes03@users.noreply.github.com>
2024-04-07 16:53:04 -0300

    dado

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography;
+
+int quantidaDeFaces;
+int resultado;
+
+Console.WriteLine("--- Simulador de Rolagem de Dado ---\n");
+
+Console.Write("Digite a quantidades de faces do dado: ");
+quantidaDeFaces = Convert.ToInt32(Console.ReadLine());
+
+resultado = RandomNumberGenerator.GetInt32(1, quantidaDeFaces + 1);
+
+Console.WriteLine($"\no resultado da rolagem do dado de {quantidaDeFaces} faces é: {resultado}");
